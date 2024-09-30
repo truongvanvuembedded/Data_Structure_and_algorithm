f5f2dbbb31e29d949576ef515643de3a7a338d66
Vu <truongvanvuvta1@gmail.com>
2024-09-30 08:02:17 +0700

    Delete text files

--- Arrays/DSA_Arrays.c
+++ Arrays/DSA_Arrays.c
@@ -1,3 +1,9 @@
+// How it works:
+
+// Go through the values in the array one by one.
+// Check if the current value is the lowest so far, and if it is, store it.
+// After looking at all the values, the stored value will be the lowest of all values in the array.
+
 #include <stdio.h>
 #include <stdlib.h> 
 
--- Arrays/DSA_Bubble_Sort.c
+++ Arrays/DSA_Bubble_Sort.c
@@ -1,3 +1,16 @@
+// How it works:
+
+// Go through the array, one value at a time.
+// For each value, compare the value with the next value.
+// If the value is higher than the next one, swap the values so that the highest value comes last.
+// Go through the array as many times as there are values in the array.
+
+// To implement the Bubble Sort algorithm in a programming language, we need:
+
+// An array with values to sort.
+// An inner loop that goes through the array and swaps values if the first value is higher than the next value. This loop must loop through one less value each time it runs.
+// An outer loop that controls how many times the inner loop must run. For an array with n values, this outer loop must run n-1 times.
+
 #include <stdio.h>
 #include <stdlib.h> 
 
